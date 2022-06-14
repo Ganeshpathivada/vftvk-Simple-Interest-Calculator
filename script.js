73f8775967dd5ff2e1074f9b14fdb3ff193b6da4
Ganeshpathivada <pathivadaganesh12@gmail.com>
2022-06-14 19:36:33 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    i = document.getElementById("rate").value;
+    years= document.getElementById("years").value;
+    
+    var si =p*i*years/100;
+  
+ 
+    document.getElementById("result").innerHTML=" If you deposit"+p+" <br> at an interest rate of "+i+"%  <br>You will receive an amount of  "+si+" ";
     
 }
         
